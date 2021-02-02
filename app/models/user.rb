7cc94a84f6af6d8ae35282725631e77795c0779b
akkun08 <akihiroaika1112@gmail.com>
2021-02-02 14:49:20 +0900

    ユーザー機能実装　修正しました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,21 +3,24 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :name, presence: true
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' } do
-    validates :japanese_last_name
-    validates :japanese_first_name
+         
+        #  validates presence:true :email, format: { with: /\A\S+@\S+\.\S+\z/}
+         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\w{6,100}\z/.freeze
+        validates_format_of  :password, presence: true, with: PASSWORD_REGEX, message: 'Password is invalid' 
+         
+         with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' } do
+          validates :japanese_last_name
+          validates :japanese_first_name
+        end
+        
+        with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters" } do
+          validates :kana_last_name
+          validates :kana_first_name
+        end
+        with_options presence: true do
+          validates :year_month_day 
+          validates :name,length: { maximum: 6 }
   end
-  
-  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters" } do
-    validates :kana_last_name
-    validates :kana_first_name
-  end
-
-  validates :year_month_day, presence: true
-
-  validates :name, presence: true, length: { maximum: 6 }
 
   has_many :items
   has_many :comments
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,12 +11,12 @@ RSpec.describe User, type: :model do
         expect(@user).to be_valid
       end
       it 'nameが6文字以内であれば登録できる' do
-        @user.name = "aaaaaa"
+        @user.name = "bbbbbb"
         expect(@user).to be_valid
       end
       it 'passwordとpassword_confirmationが6文字以上であれば登録できる' do
-        @user.password = "000000"
-        @user.password_confirmation = "000000"
+        @user.password = "000Aa0"
+        @user.password_confirmation = "000Aa0"
         expect(@user).to be_valid
       end
     end
@@ -44,13 +44,58 @@ RSpec.describe User, type: :model do
       it '重複したemailが存在する場合は登録できない ' do
         @user.save
         another_user = FactoryBot.build(:user)
-        another_user.email = @user
+        another_user.email = @user.email
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include("Email has already been taken")
       end
       it 'passwordが5文字以下は登録できない' do
         @user.password = "00000"
         @user.password_confirmation = "00000"
         @user.valid?
         expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+      end
+      it 'passwordが半角英語(6桁)のみは登録できない' do
+        @user.password = "qqqqqq"
+        @user.password_confirmation = "qqqqqq"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password Password is invalid")
+      end
+      it 'passwordが数字(6桁)のみは登録できない' do
+        @user.password = "222222"
+        @user.password_confirmation = "222222"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password Password is invalid")
+      end
+      it 'passwordが全角英数混合(6桁)は登録できない' do
+        @user.password = "１１１１１Ａ"
+        @user.password_confirmation = "１１１１１Ａ"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password Password is invalid")
+      end
+      it 'emailが@がない場合は登録できない' do
+        @user.email = "acom"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email is invalid")
+      end
+      it 'japanese_last_nameが全角(漢字、ひらがな、カタカナ)以外は登録できない' do
+        @user.japanese_last_name = "AAAAAAA"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Japanese last name Full-width characters")
+      end
+      it 'japanese_first_nameが全角(漢字、ひらがな、カタカナ)以外は登録できない' do
+        @user.japanese_first_name = "AAAAAAA"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Japanese first name Full-width characters")
+      end
+      it 'kana_last_nameが全角（カタカナ）以外は登録できない' do
+        @user.kana_last_name = "AAAAAAA"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Kana last name Full-width katakana characters")
+      end
+      it 'kana_first_nameが全角（カタカナ）以外は登録できない' do
+        @user.kana_first_name = "AAAAAAA"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Kana first name Full-width katakana characters")
       end
       it 'japanese_last_nameが空では登録できない' do
         @user.japanese_last_name = ""
