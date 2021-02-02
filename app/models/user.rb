b0885e845039ac21b49bab3c786140c5e50b7b09
akkun08 <akihiroaika1112@gmail.com>
2021-02-02 15:58:28 +0900

    ユーザー管理機能実装　修正しました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,21 +5,21 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
         #  validates presence:true :email, format: { with: /\A\S+@\S+\.\S+\z/}
-         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\w{6,100}\z/.freeze
+        PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\w{6,100}\z/.freeze
         validates_format_of  :password, presence: true, with: PASSWORD_REGEX, message: 'Password is invalid' 
         
-    width_options presence :true do
-         with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' } do
+      width_options presence: true do
+        with_options format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' } do
           validates :japanese_last_name
           validates :japanese_first_name
         end
         
-        with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters" } do
-                    validates :kana_last_name
+          with_options format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters" } do
+          validates :kana_last_name
           validates :kana_first_name
         end
 
-                      validates :year_month_day 
+          validates :year_month_day 
           validates :name,length: { maximum: 6 }
       end
     end
