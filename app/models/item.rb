c92de90f4d26247349818b9fcacce29ab2a77765
akkun08 <akihiroaika1112@gmail.com>
2021-02-03 23:12:37 +0900

    商品出品機能の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,6 +10,11 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :image
     validates :item_name, :item_detail
+    validates :category_id, numericality: { only_integer: true, greater_than_or_equal_to: 2, less_than_or_equal_to: 11 }
+    validates :shipping_charge_id, numericality: { only_integer: true, greater_than_or_equal_to: 2, less_than_or_equal_to: 3 }
+    validates :prefecture_id, numericality: { only_integer: true, greater_than_or_equal_to: 2, less_than_or_equal_to: 48 }
+    validates :state_id, numericality: { only_integer: true, greater_than_or_equal_to: 2, less_than_or_equal_to: 7 }
+    validates :delivery_id, numericality: { only_integer: true, greater_than_or_equal_to: 2, less_than_or_equal_to: 4 }
   end
 
   validates :category_id, :delivery_id, :prefecture_id, :shipping_charge_id, :state_id, numericality: { other_than: 1 }
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,17 +1,18 @@
 FactoryBot.define do
   factory :item do
-    image
     item_name { Faker::Lorem.sentence }
     item_detail { Faker::Lorem.sentence }
     category_id { 3 }
     shipping_charge_id { 2 }
     prefecture_id { 4 }
+    state_id { 4 }
     delivery_id { 2 }
     price { 333333 }
 
-    association :user
-    after(:build) do |message|
-      message.image.attach(io: File.open("public/images/test_image.jpg"), filename: "test_image.jpg")
+    # association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open("public/images/test_image.jpg"), filename: "test_image.jpg")
     end
   end
 end
