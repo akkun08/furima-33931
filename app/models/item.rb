53045e6b82964c97c55fa33878682227f85fb81c
akkun08 <akihiroaika1112@gmail.com>
2021-02-04 20:38:58 +0900

    Revert "商品詳細ページ実装"
    This reverts commit 96146104bc19de66f01d0f801fa5c9ecff5718df.

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,6 @@ class Item < ApplicationRecord
   belongs_to :shipping_charge
   belongs_to :state
   has_one_attached :image
-  has_one :purchase
 
   with_options presence: true do
     validates :image
