5b4cb15e3332921deccf8f58a6dc7faed478bcfd
akkun08 <akihiroaika1112@gmail.com>
2021-02-05 16:20:14 +0900

    商品情報編集機能の実装を編集しました。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,19 +20,15 @@ class ItemsController < ApplicationController
   end
 
   def show
-    # @item = Item.find(params[:id])
   end
 
   def edit
-    # @item = Item.find(params[:id])
     unless @item.user_id == current_user.id
       redirect_to action: :index
     end
   end
 
   def update
-    # @item = Item.find(params[:id])
-
     if @item.update(item_params)
       redirect_to item_path
     else
