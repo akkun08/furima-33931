59d3c4056e9f5cb85acb1469df0adc1fd0802eda
akkun08 <akihiroaika1112@gmail.com>
2021-02-14 16:05:26 +0900

    JavaScriptのコードを実行するページを限定する

--- app/javascript/count.js
+++ app/javascript/count.js
@@ -1,18 +1,20 @@
-function count() {
-    const itemPrice = document.getElementById("item-price");
-    itemPrice.addEventListener("keyup", () => {
-        const tax = 0.1;
-        const countVal = itemPrice.value * tax;
-        const addTaxPrice = document.getElementById("add-tax-price");
-        addTaxPrice.innerHTML = `${Math.floor(countVal).toLocaleString()}`;
-    });
+if (document.URL.match(/new/) || document.URL.match(/edit/)) {
+    function count() {
+        const itemPrice = document.getElementById("item-price");
+        itemPrice.addEventListener("keyup", () => {
+            const tax = 0.1;
+            const countVal = itemPrice.value * tax;
+            const addTaxPrice = document.getElementById("add-tax-price");
+            addTaxPrice.innerHTML = `${Math.floor(countVal).toLocaleString()}`;
+        });
 
-    itemPrice.addEventListener("keyup", () => {
-        const tax = 0.1;
-        const countVal = itemPrice.value * (1 - tax);
-        const profit = document.getElementById("profit");
-        profit.innerHTML = `${Math.round(countVal).toLocaleString()}`;
-    });
+        itemPrice.addEventListener("keyup", () => {
+            const tax = 0.1;
+            const countVal = itemPrice.value * (1 - tax);
+            const profit = document.getElementById("profit");
+            profit.innerHTML = `${Math.round(countVal).toLocaleString()}`;
+        });
+    }
+
+    window.addEventListener("load", count);
 }
-
-window.addEventListener("load", count);
